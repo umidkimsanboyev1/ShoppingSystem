8a68c162b44e2765ee5af83361d8e1f0f895e7e0
jafar <ibook2025com@gmail.com>
2022-03-28 13:14:44 +0500

    global exception handler

---
+++ src/main/java/uz/master/warehouse/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package uz.master.warehouse.exception;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import uz.master.warehouse.dto.responce.AppErrorDto;
+import uz.master.warehouse.dto.responce.DataDto;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<DataDto<AppErrorDto>> http(RuntimeException e, WebRequest request) {
+         return new ResponseEntity<>(new DataDto<>(new AppErrorDto(e.getMessage(),request, HttpStatus.OK)),HttpStatus.OK);
+    }
+
+}
