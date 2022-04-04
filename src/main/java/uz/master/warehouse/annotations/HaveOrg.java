03eb6d03e760baa3844aa5ef799a6a83dc51f37b
Shoxruh2002 <bekpulatovshoxruh9@gmail.com>
2022-04-04 17:47:05 +0500

    validator uzgardi

--- src/main/java/uz/master/warehouse/annotations/HaveOrg.java
+++ src/main/java/uz/master/warehouse/annotations/HaveOrg.java
@@ -1,17 +1,12 @@
 package uz.master.warehouse.annotations;
 
-import uz.master.warehouse.entity.organization.Organization;
-import uz.master.warehouse.enums.UniqueFieldType;
 import uz.master.warehouse.validator.organization.OrganizationValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.ElementType.TYPE_USE;
-
-@Target(value = {FIELD, ANNOTATION_TYPE,  CONSTRUCTOR, TYPE_USE})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = OrganizationValidator.class)
